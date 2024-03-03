3c4649ebae1e445e099ceb5d098569e67e8b2f2b
OttCS <ottcs20@gmail.com>
2024-03-03 14:34:08 -0500

    Something

--- scripts/all.js
+++ scripts/all.js
@@ -18,8 +18,11 @@ const ENGINE = {
         scene: false,
         textures: false,
         level: true,
+        status() {
+            return ENGINE.loaded.scene && ENGINE.loaded.textures && ENGINE.loaded.level;
+        },
         check() {
-            if (ENGINE.loaded.scene && ENGINE.loaded.textures && ENGINE.loaded.level) {
+            if (ENGINE.loaded.status()) {
                 console.log("Everything is ready!");
                 TEST_INITIALIZATION();
             }
@@ -50,17 +53,17 @@ const ENGINE = {
 // Preload all of the textures that will be needed
 ENGINE.preloadTextures([
     'assets/blank.png',
-    'assets/blank_dir.png',
     'assets/ao.png',
+    'assets/grass.png',
+    'assets/brush.png'
 ]);
 
 const SCENE = new PIXI.Application();
 SCENE.world = new PIXI.Collection(); // 3D Environment
 SCENE.ui = new PIXI.Collection(); // 2D Overlay
 
-
 // SCENE INITIALIZATION
-(async () => {
+SCENE.setup = async function () {
     await SCENE.init({
         width: screen.width,
         height: screen.height,
@@ -84,86 +87,139 @@ SCENE.ui = new PIXI.Collection(); // 2D Overlay
     SCENE.ticker.add(() => {
         SCENE.world.tick();
         SCENE.ui.tick();
+        if (ENGINE.loaded.status()) { // Engine isn't actively loading something
+            if (LEVEL.player.tick) LEVEL.player.tick();
+        }
     });
 
     ENGINE.loaded.scene = true;
     ENGINE.loaded.check();
-})();
-
-// const GAME = {};
-
-// Flags
+};
 
 // RenderType
 const VOXEL = 0;
 const BILLBOARD = 1;
 
-// LevelGroup
-const ENVIRONMENT = 0;
-const DYNAMIC = 1;
-
 // ENTITY (and Scene shortcuts for adding them)
 class Entity {
     constructor(id, x = 0, y = 0, z = 0) {
         this.sprite = new PIXI.Container();
-        this.id = id;
+        this.data = Entity.ID[id];
         this.x = x;
         this.y = y;
         this.z = z;
+        this._z = z; // Previous Z
+
+        if (Entity.ID[id].setup) Entity.ID[id].setup(this); // Specific initialization
     }
     static ID = [
-        {
+        { // Player
             id: 0,
             renderType: VOXEL,
-            levelGroup: ENVIRONMENT,
-            default: "assets/blank_dir.png"
+            dynamic: true,
+            default: "assets/blank.png",
+            setup(entity) {
+                entity.tick = function () {
+                    // console.log("I'm active!")
+                }
+            },
+        },
+        { // Grass
+            id: 1,
+            renderType: VOXEL,
+            default: "assets/grass.png"
+        },
+        { // Concrete
+            id: 2,
+            renderType: VOXEL,
+            default: "assets/brush.png"
         },
     ]
 }
 
-SCENE.world.Entity = class extends Entity {
-    constructor(id, x, y, z) {
-        super(id, x, y, z);
-        this._z = z;
-        SCENE.world.put(this);
-        this.updatePosition();
+SCENE.world.updatePosition = function () {
+    // If the z position has changed, swap the layer it's on
+    if (this.z != this._z) {
+        let newZ = this.z;
+        this.z = this._z;
+        SCENE.world.remove(this)
+        this.z = newZ;
+        SCENE.world.put(this)
     }
-    updatePosition() {
-        // If the z position has changed, swap the layer it's on
-        if (this.z != this._z) {
-            let newZ = this.z;
-            this.z = this._z;
-            SCENE.world.remove(this)
-            this.z = newZ;
-            SCENE.world.put(this)
-        }
-        [this.sprite.x, this.sprite.y] = xyzToScreen(this.x, this.y, this.z);
-        SCENE.world.layers[this.z].edit();
-        this._z = this.z; // Previous Z
-    }
+    [this.sprite.x, this.sprite.y] = xyzToScreen(this.x, this.y, this.z);
+    SCENE.world.layers[this.z].edit();
+    this._z = this.z; // Previous Z
+}
+SCENE.world.add = function (entity) {
+    SCENE.world.put(entity);
+    entity.updatePosition = SCENE.world.updatePosition;
+    return entity;
 }
 
-SCENE.ui.Entity = class extends Entity {
-    constructor(id, x, y, z) {
-        super(id, x, y, z);
-        SCENE.ui.put(this);
-        this.updatePosition();
+// SCENE.world.Entity = class extends Entity {
+//     constructor(id, x, y, z) {
+//         super(id, x, y, z);
+//         this._z = z;
+//         SCENE.world.put(this);
+//         this.updatePosition();
+//     }
+//     updatePosition() {
+//         // If the z position has changed, swap the layer it's on
+//         if (this.z != this._z) {
+//             let newZ = this.z;
+//             this.z = this._z;
+//             SCENE.world.remove(this)
+//             this.z = newZ;
+//             SCENE.world.put(this)
+//         }
+//         [this.sprite.x, this.sprite.y] = xyzToScreen(this.x, this.y, this.z);
+//         SCENE.world.layers[this.z].edit();
+//         this._z = this.z; // Previous Z
+//     }
+// }
+
+
+SCENE.ui.updatePosition = function () {
+    // If the z position has changed, swap the layer it's on
+    if (this.z != this._z) {
+        let newZ = this.z;
+        this.z = this._z;
+        SCENE.ui.remove(this)
+        this.z = newZ;
+        SCENE.ui.put(this)
     }
-    updatePosition() {
-        // If the z position has changed, swap the layer it's on
-        if (this.z != this._z) {
-            let newZ = this.z;
-            this.z = this._z;
-            SCENE.ui.remove(this)
-            this.z = newZ;
-            SCENE.ui.put(this)
-        }
-        this.sprite.x = this.x;
-        this.sprite.y = this.y;
-        SCENE.ui.layers[this.z].edit();
-        this._z = this.z; // Previous Z
-    }
+    this.sprite.x = this.x;
+    this.sprite.y = this.y;
+    SCENE.ui.layers[this.z].edit();
+    this._z = this.z; // Previous Z
 }
+SCENE.ui.add = function (entity) {
+    SCENE.ui.put(entity);
+    entity.updatePosition = SCENE.ui.updatePosition;
+    return entity;
+}
+
+// SCENE.ui.Entity = class extends Entity {
+//     constructor(id, x, y, z) {
+//         super(id, x, y, z);
+//         SCENE.ui.put(this);
+//         this.updatePosition();
+//     }
+//     updatePosition() {
+//         // If the z position has changed, swap the layer it's on
+//         if (this.z != this._z) {
+//             let newZ = this.z;
+//             this.z = this._z;
+//             SCENE.ui.remove(this)
+//             this.z = newZ;
+//             SCENE.ui.put(this)
+//         }
+//         this.sprite.x = this.x;
+//         this.sprite.y = this.y;
+//         SCENE.ui.layers[this.z].edit();
+//         this._z = this.z; // Previous Z
+//     }
+// }
 
 // Produces skewed Sprites to fit an isometric voxel's faces
 const FACE = {
@@ -173,7 +229,7 @@ const FACE = {
             this.skew.x = 4 * Math.PI / 3;
             this.skew.y = -Math.PI / 6;
             this.height = this.width = UNIT;
-            this.tint = 0x0000ff;
+            this.tint = 0xffffff;
         }
     },
     Left: class extends PIXI.Sprite {
@@ -181,7 +237,7 @@ const FACE = {
             super(ENGINE.assets.get(url));
             this.skew.y = 7 * Math.PI / 6;
             this.height = this.width = UNIT;
-            this.tint = 0xff0000;
+            this.tint = 0xbfdfff;
         }
     },
     Right: class extends PIXI.Sprite {
@@ -189,7 +245,7 @@ const FACE = {
             super(ENGINE.assets.get(url));
             this.skew.y = -Math.PI / 6;
             this.height = this.width = UNIT;
-            this.tint = 0x00ff00;
+            this.tint = 0xdfefff;
         }
     },
     Down: class extends PIXI.Sprite {
@@ -198,7 +254,7 @@ const FACE = {
             this.skew.x = -Math.PI / 3;
             this.skew.y = Math.PI / 6;
             this.height = this.width = UNIT;
-            this.tint = 0xffff00;
+            this.tint = 0xffffff;
         }
     },
     BLeft: class extends PIXI.Sprite { // Back Left
@@ -235,14 +291,15 @@ const LEVEL = {
     environment: new SpatialHash(),
     dynamic: new Set(),
     name: 'level',
+    player: { x: 5, y: 5, z: 0 },
     // getLevelBackendLocation(entity) {
     //     return entity.id < 128 ? this.environment : this.dynamic;
     // },
     create(id, x, y, z, renderImmediate = false) {
-        const entity = new SCENE.world.Entity(id, x, y, z);
+        const entity = SCENE.world.add(new Entity(id, x, y, z));
 
         // Add it to the proper collection
-        (Entity.ID[entity.id].levelGroup == ENVIRONMENT ? this.environment : this.dynamic).add(entity);
+        (entity.data.dynamic ? this.dynamic : this.environment).add(entity);
 
         // // If we need it right away, do that
         if (renderImmediate)
@@ -252,34 +309,36 @@ const LEVEL = {
     },
     // Post-culled voxel face list
     // renderFaces: ENGINE.backCulling ? ['Up', 'Left', 'Right'] : ['BLeft', 'BRight', 'Down', 'Up', 'Left', 'Right'],
-    renderSingle(entity) {
-        let data = Entity.ID[entity.id];
-        if (data.renderType == VOXEL) { // Voxel
+    renderSingle(entity, log) {
+        if (entity.data.renderType == VOXEL) { // Voxel
 
-            entity.Up = new FACE.Up(data.Up || data.default);
+            entity.Up = new FACE.Up(entity.data.Up || entity.data.default);
             entity.sprite.addChild(entity.Up);
 
-            entity.Left = new FACE.Left(data.Left || data.default);
+            entity.Left = new FACE.Left(entity.data.Left || entity.data.default);
             entity.sprite.addChild(entity.Left);
 
-            entity.Right = new FACE.Right(data.Right || data.default);
+            entity.Right = new FACE.Right(entity.data.Right || entity.data.default);
             entity.sprite.addChild(entity.Right);
-            // for (const f of this.renderFaces) {
 
-            //     // Eventually add proper "Meshing" to reduce sprite number
-            //     entity[f] = new FACE[f](data[f] || data.default);
-            //     entity.sprite.addChild(entity[f]);
-            // }
+            // Eventually add proper "Meshing" to reduce sprite number
+
         } else { // Billboard
 
         }
+        if (log) console.log(entity);
     },
     render() {
+        this.renderSingle(LEVEL.player, true);
         for (let [k, entity] of LEVEL.environment)
+            this.renderSingle(entity);
+        for (let [k, entity] of LEVEL.dynamic)
             this.renderSingle(entity);
     },
     import(url) {
         ENGINE.loaded.level = false;
+
+        let temPlayer = SCENE.world.add(new Entity(0, LEVEL.player.x, LEVEL.player.y, LEVEL.player.z));
 
         fetch(url).then(r => r.blob()).then(blob => {
             // Clear for new scene
@@ -314,9 +373,14 @@ const LEVEL = {
                         BIN[pointer + 1] - 128,
                         BIN[pointer + 2] - 128,
                         BIN[pointer + 3]
-                    );
+                    ).updatePosition();
                     pointer += 4;
                 }
+
+                LEVEL.player = temPlayer;
+                SCENE.world.target = temPlayer.sprite;
+                temPlayer.updatePosition();
+                SCENE.world.add(temPlayer);
 
                 ENGINE.loaded.level = true;
                 LEVEL.render();
@@ -333,7 +397,7 @@ const LEVEL = {
         BIN[i++] = 0x56;
         BIN[i++] = 0x58;
         BIN[i++] = 0x46;
-        BIN[i++] = VXF_VERSION & 0xff;
+        BIN[i++] = VXF_VERSION;
         i = HEADER_SIZE;
         LEVEL.environment.forEach(v => {
             BIN[i++] = v.id;
@@ -355,17 +419,32 @@ const LEVEL = {
     }
 }
 
+let loadLevel = true;
+
 // TESTING FUNCTION
 async function TEST_INITIALIZATION() {
 
-    // for (let z = 0; z < 3; z++) {
-    //     LEVEL.create(0, 0, 0, z);
-    //     for (let x = -2; x <= 2; x++) {
-    //         for (let y = -2; y <= 2; y++) {
-    //             if (Math.random() > 0 + 0.3 * z)
-    //                 LEVEL.create(0, x, y, z);
-    //         }
+    if (loadLevel) {
+        loadLevel = false;
+        LEVEL.import('level/demoBox.vxf');
+    }
+
+    // let z = 0;
+    // // for (let z = 0; z < 3; z++) {
+    // //     LEVEL.create(0, 0, 0, z);
+    // let ring = 6;
+    // for (let x = -ring; x <= ring; x++) {
+    //     for (let y = -ring; y <= ring; y++) {
+    //         LEVEL.create(1, x, y, z);
+    //         if (x == -ring || x == ring || y == -ring || y == ring)
+    //             LEVEL.create(2, x, y, z + 1);
+
+    //         if (Math.random() * 4 > Math.hypot(x, y))
+    //             LEVEL.create(2, x, y, z + 1);
     //     }
     // }
+    // // }
     // LEVEL.render();
 };
+
+SCENE.setup();
